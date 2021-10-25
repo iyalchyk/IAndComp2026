114fc73462856ecec9c9acc7b6f96c65c94b76ba
Ilya Yalchyk <yalchik.ilya@gmail.com>
2021-10-25 19:49:49 +0200

    added housing panel

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ const Player = {
     housing: {
         apartment: null,
         furniture: null,
+        kitchen: null,
+        bathroom: null
     }
 };
 
@@ -54,28 +56,67 @@ $(function (events, handler) {
     }
 
     function housing_button_handler() {
-        $(".switchable").hide()
-        $("#housing_panel").show()
-        $("#home_button").show()
+        $(".switchable").hide();
+        $("#housing_panel").show();
+        $("#home_button").show();
+    }
+
+    function buy_apartment_button_handler() {
+        let next_apartment_id = Player.housing.apartment["id"] + 1;
+        let next_apartment = Shop["apartments"][next_apartment_id];
+        if (Player.money < next_apartment["price"]) {
+            alert("No money")
+            return
+        }
+        Player.money -= next_apartment["price"];
+        Player.housing.apartment = next_apartment;
+        $("#apartment").text(next_apartment["description"]);
+        $("#money").text(Player.money);
+        if (next_apartment_id + 1 == Shop["apartments"].length) {
+            // no more apartments
+            $("#buy_apartment_button").prop('disabled', true);
+        }
+        home_button_handler()
+    }
+
+    function buy_apartment_button_mouseenter_handler() {
+        console.log("!!!")
+        let next_apartment_id = Player.housing.apartment["id"] + 1;
+        let next_apartment = Shop["apartments"][next_apartment_id];
+        if (next_apartment) {
+            $("#housing_panel_price_label").text(next_apartment["price"]);
+        }
+        else {
+            $("#housing_panel_price_label").text("-");
+        }
+
     }
 
     function home_button_handler() {
-        $(".switchable").hide()
-        $("#status_panel").show()
-        $("#home_button").hide()
+        $(".switchable").hide();
+        $("#status_panel").show();
+        $("#home_button").hide();
     }
 
     $.getJSON("js/assortment.json", function(data) {
         Shop = data;
-        Player.housing.apartment = Shop.apartment[0];
-        Player.housing.furniture = Shop.furniture[0];
+        Player.housing.apartment = Shop["apartments"][0];
+        Player.housing.furniture = Shop["furniture"][0];
+        Player.housing.kitchen = Shop["kitchens"][0];
+        Player.housing.bathroom = Shop["bathrooms"][0];
         console.log(Player);
-        }).fail(function() {
-            console.log("An error has occurred.");
+        }).fail(function(e, e2) {
+            console.log("An error has occurred.", e, e2);
         });
 
     $("#housing_button").on("click", housing_button_handler)
+    $("#buy_apartment_button").on("click", buy_apartment_button_handler)
     $("#home_button").on("click", home_button_handler)
+
+    $("#buy_apartment_button").on("mouseenter", buy_apartment_button_mouseenter_handler)
+    $("#buy_apartment_button").on("mouseleave", function() {
+        $("#housing_panel_price_label").text("-");
+    })
     home_button_handler()
 
     update_world_state();
